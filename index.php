a4365791c45b20fb74d3715aaceafc9e44279a0f
Diego Mendes <mendes87.diego@gmail.com>
2016-12-06 18:03:38 -0200

    Atualizando Arquivos

--- index.php
+++ index.php
@@ -92,19 +92,139 @@
 
     <section class="sobre">
       <div class="container">
-        <div class="col-md-6">
-          <h2 class="titulo-section">Sobre<br> a Empresa</h2>
-          <p class="texto-sobre">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div class="col-md-6">
-          <img src="images/img.jpg" alt="" class="img-responsive center-block">
+        <div class="row">
+          <div class="col-md-6">
+            <h2 class="titulo-section">Sobre<br> a Empresa</h2>
+            <p class="texto-sobre">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div class="col-md-6">
+            <img src="images/img.jpg" alt="" class="img-responsive center-block">
+          </div>
+          <p align="center"><a class="btn btn-section" href="#" role="button">Conheça nossa faculdade</a></p>
         </div>
       </div>
     </section>
 
+    <section class="curso-home">
+      <div class="container">
+        <h2 class="titulo-section">Cursos<br> em destaque</h2>
+        <p class="texto-sobre">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod<br> tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam
+        </p>
+
+        <div class="row">
+          <div class="col-md-3">
+            <img src="images/banner-destaque.jpg" alt="" class="img-responsive banner-lateral">
+          </div>
+          <div class="col-md-9">
+            <div class="row">
+              <div class="col-md-4">
+                <div class="box-curso">
+                  <img src="images/07.jpg" alt="" class="img-responsive">
+                  <h3 class="titulo-curso">Titulo Curso</h3>
+                  <p class="desc-curso">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                  <p align="center"><a class="btn btn-curso-home" href="#" role="button">Saiba Mais</a></p>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="box-curso">
+                  <img src="images/06.jpg" alt="" class="img-responsive">
+                  <h3 class="titulo-curso">Titulo Curso</h3>
+                  <p class="desc-curso">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                  <p align="center"><a class="btn btn-curso-home" href="#" role="button">Saiba Mais</a></p>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="box-curso">
+                  <img src="images/05.jpg" alt="" class="img-responsive">
+                  <h3 class="titulo-curso">Titulo Curso</h3>
+                  <p class="desc-curso">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                  <p align="center"><a class="btn btn-curso-home" href="#" role="button">Saiba Mais</a></p>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-4">
+                <div class="box-curso">
+                  <img src="images/07.jpg" alt="" class="img-responsive">
+                  <h3 class="titulo-curso">Titulo Curso</h3>
+                  <p class="desc-curso">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                  <p align="center"><a class="btn btn-curso-home" href="#" role="button">Saiba Mais</a></p>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="box-curso">
+                  <img src="images/06.jpg" alt="" class="img-responsive">
+                  <h3 class="titulo-curso">Titulo Curso</h3>
+                  <p class="desc-curso">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                  <p align="center"><a class="btn btn-curso-home" href="#" role="button">Saiba Mais</a></p>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="box-curso">
+                  <img src="images/05.jpg" alt="" class="img-responsive">
+                  <h3 class="titulo-curso">Titulo Curso</h3>
+                  <p class="desc-curso">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                  <p align="center"><a class="btn btn-curso-home" href="#" role="button">Saiba Mais</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p align="center"><a class="btn btn-section" href="#" role="button">Ver todos os cursos</a></p>
+      </div>
+    </section>
+
+    <section class="blog-home">
+      <div class="container">
+        <h2 class="titulo-section">Últimas Notícias</h2>
+        <p class="texto-sobre">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod<br> tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam
+        </p>
+
+        <div class="row">
+          <div class="col-md-3">
+            <div class="box-noticia">
+              <img src="images/d.jpg" alt="" class="img-responsive">
+              <h3 class="titulo-noticia-home">Titulo Notícia</h3>
+              <span class="info-add-home">06 <small>Dez</small></span>
+              <p class="desc-curso">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="box-noticia">
+              <img src="images/c.jpg" alt="" class="img-responsive">
+              <h3 class="titulo-noticia-home">Titulo Notícia</h3>
+              <span class="info-add-home">06 <small>Dez</small></span>
+              <p class="desc-curso">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="box-noticia">
+              <img src="images/b.jpg" alt="" class="img-responsive">
+              <h3 class="titulo-noticia-home">Titulo Notícia</h3>
+              <span class="info-add-home">06 <small>Dez</small></span>
+              <p class="desc-curso">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="box-noticia">
+              <img src="images/a.jpg" alt="" class="img-responsive">
+              <h3 class="titulo-noticia-home">Titulo Notícia</h3>
+              <span class="info-add-home">06 <small>Dez</small></span>
+              <p class="desc-curso">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+            </div>
+          </div>
+        </div>
+        <p align="center"><a class="btn btn-noticia-home" href="#" role="button">Ver todas</a></p>
+      </div>
+    </section>
 
   </body>
 </html>
